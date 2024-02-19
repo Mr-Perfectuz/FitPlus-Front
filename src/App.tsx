7ef1bf4375b689f5d5c0b8db259e11d57e954e69
Mr-Perfectuz <ziyodovanvar1999@example.com>
2024-02-19 13:32:48 +0900

    feat update feature data

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,73 @@ import React from "react";
 import "./App.css";
 import { Box, Container, Stack, Typography, Button } from "@mui/material";
 import { RippleBadge } from "./app/MaterialTheme/styled";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Container maxWidth="sm">
-      <Stack>
-        <Box sx={{ my: 4 }}>
-          <Typography variant="h4" component={"h1"}>
-            {" "}
-            React App
-          </Typography>
-        </Box>
-        <Box>
-          <RippleBadge badgeContent={4}>
-            <Button variant="contained">Contained</Button>
-          </RippleBadge>
-        </Box>
-      </Stack>
-    </Container>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/member-page">MemberPage</Link>
+            </li>
+            <li>
+              <Link to="/stores">Stores</Link>
+            </li>
+            <li>
+              <Link to="/blogs">Blogs</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/products">
+            <Users />
+          </Route>
+          <Route path="/stores">
+            <Home />
+          </Route>
+          <Route path="/member-page">
+            <Home />
+          </Route>
+          <Route path="/blogs">
+            <Home />
+          </Route>
+          <Route path="/contact">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
